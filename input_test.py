b1e8666816696d54c82a6c5fbe8f2559e1c23542
LegendNeo42 <niklasdiesel24@gmail.com>
2025-01-13 15:19:07 +0100

    changeInputTest

--- input_test.py
+++ input_test.py
@@ -1,19 +1,23 @@
+import keyboard
+
+answer = 0
+
 # Participant ID eingeben
 participant_id = input("Gib die Participant ID ein: ")
 
-# Instrument auswählen
-instrument = input("Welches Instrument wird verwendet? (gitarre, keyboard, drums): ")
 
 # Eingaben bestätigen
 print(f"Participant ID: {participant_id}")
-print(f"Instrument: {instrument}")
+def on_e_pressed():
+    global answer 
+    answer = 4
+    print("InMtehodE")
+    print(answer)
 
-# Python-Logik basierend auf den Eingaben
-if instrument == "gitarre":
-    print("Du spielst Gitarre.")
-elif instrument == "keyboard":
-    print("Du spielst Keyboard.")
-elif instrument == "drums":
-    print("Du spielst Drums.")
-else:
-    print("Unbekanntes Instrument.")
+def on_f_pressed():
+    print(answer)
+
+keyboard.on_press_key("e", lambda _: on_e_pressed())
+keyboard.on_press_key("f", lambda _: on_f_pressed())
+while True:
+    continue
