9eae5d19348ef1f355886c01c1407985600ab6e8
Niklas Keller <niklaskel99@gmail.com>
2025-01-14 16:54:22 +0100

    Add Logging Test mit weirden Bugs

--- logging_test.py
+++ logging_test.py
@@ -1,7 +1,4 @@
 #TO DO:
-# Bei Tastendruck soll geloggt werden
-# den Code aus dem Pi übernehmen, mit Variationen für das Instrument 1,2 und 3
-# current_instrument mitloggen
 # Gewichtete Randomness bei e unf f Taste Latenz 50/50
 # runtime_total muss noch geloggt werden
 # count muss noch geloggt werden
@@ -9,31 +6,15 @@
 # runtime_total muss noch geloggt werden (und ein Array werden, 1 Wert pro Instrument)
 # latency_results muss noch geloggt werden
 # Logging anpassen
-# Cooldown auf Press falls person zu lange auf Fußpedal ist (gerade ca 1 Sekunde)
 
 import time # time delay
-import RPi.GPIO as GPIO
 import sys # for command line parameters
-import threading
 import random
 import pandas as pd
 import keyboard
 
-
-# select broadcom pin numbers: https://pinout.xyz/#
-GPIO.setmode(GPIO.BCM)
-
-# set pins to output mode, Pin 18 ist für den Oscillator
-GPIO.setup(18, GPIO.OUT)
-GPIO.setup(23, GPIO.OUT)
-
-# set pins to input mode, Pin 2 und 3 sind jeweils für die Tasten E und F, aber auch nur für den elektrischen Current mit dem Kupfer
-GPIO.setup(2, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-GPIO.setup(3, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-
-# initialize parameters
-pin_2_value = 1
-pin_3_value = 1
+# diese Variable wird auf True gesetzt, sobald alle Versuche durchgelaufen sind, damit das Programm beendet wird
+stop_loop = False
 
 # constant parameter for decision
 KEY_E = 1
@@ -46,6 +27,7 @@ counter_f = 0
 latency_e = 0
 latency_f = 0
 correct_side = ""
+last_correct_answer = 0.256
 
 # sum of both counters
 count = 0
@@ -56,13 +38,14 @@ random_key = random.randint(0, 1)
 
 # parameters for latency
 latency = 0.256
-latency_step = 0 # starts with 128 -> 64 -> 32 -> 16 -> 8 -> 4 -> 2 -> 1
+latency_step = [0.128, 0.064, 0.032, 0.016, 0.008, 0.004, 0.002, 0.001] # starts with 128 -> 64 -> 32 -> 16 -> 8 -> 4 -> 2 -> 1
+
 
 # Ein trial beschreibt eine 10er-Runde an reps. Nach jedem Trial wird die Latenz verändert. Insgesamt gibt es pro instrument 8 trials.
-trial = 1
+trial = 0
 
 # rep beschreibt einen Unterscheidungs-Versuch. 10 reps ergeben einen trial.
-rep = 1
+rep = 0
 
 # runtime beschreibt die Zeit für einen rep.
 runtime = 0
@@ -110,9 +93,11 @@ current_instrument_number = 0
 current_instrument = instrument_order[current_instrument_number]
 
 def on_e_pressed():
+    print("Key pressed:     E")
     handle_foot_input(KEY_E)
 
 def on_f_pressed():
+    print("Key pressed:     F")
     handle_foot_input(KEY_F)
 
 # Bekomme hier den gepressten Fußpedal-Key übergeben
@@ -124,6 +109,7 @@ def handle_foot_input(key):
     global runtime
     global runtime_results
     global rep
+    global last_correct_answer
 
     #Prüfe, ob die Eingabe richtig war (KEY_E oder KEY_F)
     answer = checkInput(key)
@@ -136,10 +122,10 @@ def handle_foot_input(key):
 
     if (key == KEY_E):
         # Logge den ganzen Stuff MIT DER TASTE E
-        log_data_pair.append({"instrument" : current_instrument, "repetition" : rep, "trial" : trial, "counter_e" : counter_e, "counter_f" : counter_f, "latency_e" : latency_e, "latency_f" : latency_f, "key" : "e", "correct_side" : correct_side, "answer" : answer, "time" : runtime})
+        log_data_pair.append({"instrument" : current_instrument, "repetition" : rep, "trial" : trial, "counter_e" : counter_e, "counter_f" : counter_f, "latency_e" : latency_e, "latency_f" : latency_f, "key" : "e", "correct_side" : correct_side, "answer" : answer, "time" : runtime, "last_correct_answer" : last_correct_answer})
     else:
         # Logge den ganzen Stuff MIT DER TASTE F
-        log_data_pair.append({"instrument" : current_instrument, "repetition" : rep, "trial" : trial, "counter_e" : counter_e, "counter_f" : counter_f, "latency_e" : latency_e, "latency_f" : latency_f, "key" : "f", "correct_side" : correct_side, "answer" : answer, "time" : runtime})
+        log_data_pair.append({"instrument" : current_instrument, "repetition" : rep, "trial" : trial, "counter_e" : counter_e, "counter_f" : counter_f, "latency_e" : latency_e, "latency_f" : latency_f, "key" : "f", "correct_side" : correct_side, "answer" : answer, "time" : runtime, "last_correct_answer" : last_correct_answer})
     # Setze die Werte für E und F zurück
     resetCounter()
     # Setze die Latenzen neu, und lasse die Konsole das Ergebnis ausgeben. 
@@ -151,6 +137,7 @@ def handle_foot_input(key):
 # answer = 1: input is true
 # answer = 0: input is false
 def setLatency(answer):
+    global stop_loop
     global trial
     global latency_step
     global latency
@@ -160,16 +147,18 @@ def setLatency(answer):
     global percent
     global runtime_results
     global current_instrument
+    global current_instrument_number
+    global last_correct_answer
     
-    print(f'trial;{trial}')
-    print(f"rep;{rep}")
+    print("Das ist "f'trial:   {trial}')
+    print("Das ist "f"rep:     {rep}")
 
     rep += 1
     # randomized zwischen E und F
     setRandomKey()
     
     # Solange noch nicht der 10. Versuch war...
-    if(rep <= 10):
+    if(rep < 10):
         if answer == 1:
             percent +=1
             print("richtig")
@@ -184,19 +173,19 @@ def setLatency(answer):
 
         elif answer == 0:
             print("falsch")
-        
-        # Sollte eine 10ner-Runde beendet sein, update die nötigen Counter und wende die 8/10-PEST plus oder minus an.
-        trial +=1
-        rep = 1
-        setLatencyStep(trial)
+    
         
         # Warum wird percent nicht im else zurückgesetzt?
         if(percent >= 8):
-            latency = latency - latency_step
+            latency = latency - latency_step[trial]
+            last_correct_answer = latency
         else:
-            latency = latency + latency_step
+            latency = latency + latency_step[trial]
         # Hier ausgeschachtelt, war vorher nur im if
         percent = 0
+        # Sollte eine 10ner-Runde beendet sein, update die nötigen Counter und wende die 8/10-PEST plus oder minus an.
+        trial +=1
+        rep = 0
     
     # Im Falle der letzten Runde der letzten Runde
     else:
@@ -208,14 +197,16 @@ def setLatency(answer):
         # Ist es nötig, die Werte zu resetten wenn das Programm eh beendet wird?
         runtime_total = 0
         runtime_results = []
+        
         if(current_instrument_number < 2):
+            print("CURRENT_INSTRUMENT_NUMBER: " + str(current_instrument_number))
             current_instrument_number += 1
             current_instrument = instrument_order[current_instrument_number]
             reset_values()
         else:
             saveLog()
-    # Anstatt flush vielleicht beenden?    
-    sys.stdout.flush()
+            stop_loop = True
+
 
 def reset_values():
     global latency
@@ -226,8 +217,8 @@ def reset_values():
 
     latency = 0.256
     count = 0
-    trial = 1
-    rep = 1
+    trial = 0
+    rep = 0
     percent = 0
 
 
@@ -274,10 +265,6 @@ def setRandomKey():
     global random_key
     random_key = random.randint(0, 1)
 
-# calculates and sets latency_step depending on trial
-def setLatencyStep(trial):
-    global latency_step
-    latency_step = 0.256 / (2**trial)
 
 # saves csv files
 def saveLog():
@@ -302,56 +289,10 @@ def resetCounter():
     counter_f = 0
 
 
-# DAS HIER SIND DIE CALLBACK METHODEN! HIER MUSS NOCH SHIT REIN! UND RAUS!
-# callback key e
-def callback_pin_2 (*args):
-    global pin_2_value
-    global latency
-    global e_timer
-    
-    state = not GPIO.input(2)
-    pin_2_value = state
-    
-    if (state == True and (time.time() - e_timer < 0.02)):
-        return
-    e_timer = time.time()
-    
-    if(random_key == 1):
-        threading.Thread(target = play_tone, args = (state, latency,), daemon = True).start()
-    else:
-        threading.Thread(target = play_tone, args = (state, 0), daemon = True).start()
-# callback key f
-def callback_pin_3 (*args):
-    global pin_3_value
-    global latency
-    global f_timer
-    
-    state = not GPIO.input(3)
-    pin_3_value = state
-    
-    if (state == True and (time.time() - f_timer < 0.02)):
-        return
-    f_timer = time.time()
-    
-    if(random_key == 0):
-        threading.Thread(target = play_tone, args = (state, latency,), daemon = True).start()
-    else:
-        threading.Thread(target = play_tone, args = (state, 0), daemon = True).start()
-
-
-#KANN MAN WAHRSCHEINLICH UMSCHREIBEN, so dass nur bei der "correct_side" ein sleep passiert
-# function for playing tone with different latency
-def play_tone (state, latency):
-    time.sleep(latency)
-    GPIO.output(18, state)
-
-
-# detect input from GPIO and keyboard note e1 and f1
-GPIO.add_event_detect(2, edge = GPIO.BOTH, callback=callback_pin_2)
-GPIO.add_event_detect(3, edge = GPIO.BOTH, callback=callback_pin_3)
-
 keyboard.on_press_key("e", lambda _: on_e_pressed())
 keyboard.on_press_key("f", lambda _: on_f_pressed())
 
 while True:
-    continue
+    if stop_loop:
+        sys.stdout.flush()
+        break
