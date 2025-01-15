c955a63815af9dfa398eabec750c85ddd33b5a69
LegendNeo42 <niklasdiesel24@gmail.com>
2025-01-15 16:02:10 +0100

    Code finalized for transferring into a new master python file that will be transferred to the Raspberry Pi.

--- midi_print_april_NEWandSHINY.py
+++ midi_print_april_NEWandSHINY.py
@@ -1,9 +1,7 @@
 #TO DO:
-# den Code aus dem Pi übernehmen, mit Variationen für das Instrument 1,2 und 3
 # Logging anpassen im Bezug auf runtime (rohe Time loggen)
-# Cooldown auf Press falls person zu lange auf Fußpedal ist (gerade ca 1 Sekunde)
-# Bei Gitarre Code NOT ändern mit If abfrage
-# Code aufräumen (Kommentare ändern, Prints verändern)
+# Cooldown auf die Fußpedale
+# Callback-Methoden: bei Gitarre Code NOT ändern mit If abfrage
 # GUI Bauen
 
 import time # time delay
@@ -14,19 +12,19 @@ import random
 import pandas as pd
 import keyboard
 
-# diese Variable wird auf True gesetzt, sobald alle Versuche durchgelaufen sind, damit das Programm beendet wird
-stop_loop = False
-
 # select broadcom pin numbers: https://pinout.xyz/#
 GPIO.setmode(GPIO.BCM)
 
 # set pins to output mode, Pin 18 ist für den Oscillator
 GPIO.setup(18, GPIO.OUT)
-GPIO.setup(23, GPIO.OUT)
+#GPIO.setup(23, GPIO.OUT)
 
 # set pins to input mode, Pin 2 und 3 sind jeweils für die Tasten E und F, aber auch nur für den elektrischen Current mit dem Kupfer
 GPIO.setup(2, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 GPIO.setup(3, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+
+# diese Variable wird auf True gesetzt, sobald alle Versuche durchgelaufen sind, damit das Programm beendet wird
+stop_loop = False
 
 # initialize parameters
 #pin_2_value = 1
@@ -42,12 +40,11 @@ counter_f = 0
 
 latency_e = 0
 latency_f = 0
-# Korrekt ist die Seite mit Latenz
-correct_side = ""
-last_correct_answer = 0
+# latency_side ist der Key der Latenz drauf hat
+latency_side = ""
+# pb = personal_best
+current_pb_latency = 0
 
-# sum of both counters
-count = 0
 # answer = 1
 
 # random generator for key assignment
@@ -60,32 +57,42 @@ latency_step = [0.064, 0.032, 0.016, 0.008, 0.004, 0.002, 0.001]
 
 # Ein trial beschreibt eine 10er-Runde an reps. Nach jedem Trial wird die Latenz verändert. Insgesamt gibt es pro instrument 8 trials.
 trial = 0
+#trials per instrument
+trials_per_instrument = 8
 
+# number of instruments
+number_of_instruments = 3
 # rep beschreibt einen Unterscheidungs-Versuch. 10 reps ergeben einen trial.
 rep = 0
+#reps per trial
+reps_per_trial = 10
+
+# diese Anzahl an Antworten müssen mindestens korrekt sein
+min_correct_answers = 8 #All Caps
 
 # runtime beschreibt die Zeit für einen rep.
-runtime = 0
+#runtime = 0
 # In diesem Array werden die runtimes gespeichert
-runtime_results = []
+#runtime_results = []
 # Am Ende wird die durchschnittliche runtime errechnet
-runtime_average = 0
+#runtime_average = 0
 # Hier werden die Gesamtzeiten pro Instrument gespeichert
-runtime_total = []
+#runtime_total = []
 #final_result = 0
 
 # Anzahl der richtigen Eingaben je 10ner-Runde. Sollten nach 10 Eingaben mind. 8 richtig sein (also percent größer gleich 8), dann yippie.
-percent = 0
+correct_answers = 0
 
-e_timer = time.time()
-f_timer = time.time()
+
+last_key_press = 0
+COOLDOWN = 0.1
 
 
 # parameters for saving data
 log_data_pair = []
 
 # Warum ist das ein Array?
-latency_results = []
+#latency_results = []
 
 
 # verschiedene Kombinationsmöglichkeiten (latin square)
@@ -121,12 +128,11 @@ def on_f_pressed():
 def handle_foot_input(key):
     global counter_e
     global counter_f
-    global count
     global trial
     global runtime
     global runtime_results
     global rep
-    global last_correct_answer
+    global current_pb_latency
     global log_data_pair
 
     #Prüfe, ob die Eingabe richtig war (KEY_E oder KEY_F)
@@ -140,10 +146,10 @@ def handle_foot_input(key):
 
     if (key == KEY_E):
         # Logge den ganzen Stuff MIT DER TASTE E
-        log_data_pair.append({"instrument" : current_instrument, "repetition" : rep, "trial" : trial, "counter_e" : counter_e, "counter_f" : counter_f, "latency_e" : latency_e, "latency_f" : latency_f, "key" : "e", "correct_side" : correct_side, "answer" : answer, "time" : runtime, "current_personal_best_latency" : last_correct_answer})
+        log_data_pair.append({"instrument" : current_instrument, "repetition" : rep, "trial" : trial, "counter_e" : counter_e, "counter_f" : counter_f, "latency_e" : latency_e, "latency_f" : latency_f, "key" : "e", "correct_side" : latency_side, "answer" : answer, "time" : runtime, "current_personal_best_latency" : current_pb_latency})
     else:
         # Logge den ganzen Stuff MIT DER TASTE F
-        log_data_pair.append({"instrument" : current_instrument, "repetition" : rep, "trial" : trial, "counter_e" : counter_e, "counter_f" : counter_f, "latency_e" : latency_e, "latency_f" : latency_f, "key" : "f", "correct_side" : correct_side, "answer" : answer, "time" : runtime, "current_personal_best_latency" : last_correct_answer})
+        log_data_pair.append({"instrument" : current_instrument, "repetition" : rep, "trial" : trial, "counter_e" : counter_e, "counter_f" : counter_f, "latency_e" : latency_e, "latency_f" : latency_f, "key" : "f", "correct_side" : latency_side, "answer" : answer, "time" : runtime, "current_personal_best_latency" : current_pb_latency})
    # Setze die Werte für E und F zurück
     resetCounter()
     # Setze die Latenzen neu, und lasse die Konsole das Ergebnis ausgeben. 
@@ -166,7 +172,8 @@ def setLatency(answer):
     global runtime_results
     global current_instrument
     global current_instrument_number
-    global last_correct_answer
+    global current_pb_latency
+    global min_correct_answers
     
     print("Das ist "f'trial:   {trial}')
     print("Das ist "f"rep:     {rep}")
@@ -176,7 +183,7 @@ def setLatency(answer):
     setRandomKey()
     
     # Solange noch nicht der 10. Versuch war...
-    if(rep < 10):
+    if(rep < reps_per_trial):
         if answer == 1:
             percent +=1
             print("richtig")
@@ -184,16 +191,16 @@ def setLatency(answer):
         elif answer == 0:
             print("falsch")
     # ... und noch nicht die neunte Runde (PEST!), dann zeige einfach nur wahr oder falsch an und update ggf. den percent-(Anzahl richtiger Eingaben)-Tracker.
-    elif (trial < 8):
+    elif (trial < trials_per_instrument):
         if answer == 1:
-            percent +=1
+            percent += 1
             print("richtig")
 
         elif answer == 0:
             print("falsch")
         
-        if(percent >= 8):
-            last_correct_answer = latency
+        if(percent >= min_correct_answers):
+            current_pb_latency = latency
             print(latency)
             print("minus")
             print(latency_step[trial])
@@ -214,7 +221,7 @@ def setLatency(answer):
 
         percent = 0
         # Sollte eine 10ner-Runde beendet sein, update die nötigen Counter und wende die 8/10-PEST plus oder minus an.
-        trial +=1
+        trial += 1
         rep = 0
     
     # Im Falle der letzten Runde der letzten Runde
@@ -227,7 +234,7 @@ def setLatency(answer):
         # Ist es nötig, die Werte zu resetten wenn das Programm eh beendet wird?
         runtime_total = 0
         runtime_results = []
-        if(current_instrument_number < 2):
+        if(current_instrument_number < (number_of_instruments - 1)):
             current_instrument_number += 1
             print("CURRENT_INSTRUMENT_NUMBER: " + str(current_instrument_number))
             current_instrument = instrument_order[current_instrument_number]
@@ -239,19 +246,17 @@ def setLatency(answer):
 
 def reset_values():
     global latency
-    global count
     global trial
     global rep
     global percent
-    global last_correct_answer
+    global current_pb_latency
     global latency_keys
 
     latency = 0.256
-    count = 0
     trial = 0
     rep = 0
     percent = 0
-    last_correct_answer = 0
+    current_pb_latency = 0
     random.shuffle(latency_keys)
 
 
@@ -280,18 +285,18 @@ def getLatencies():
     global latency_keys
     global latency_e
     global latency_f
-    global correct_side
+    global latency_side
     global trial
     global rep
     
-    if(latency_keys[10*trial + rep] == "E"):
+    if(latency_keys[reps_per_trial*trial + rep] == "E"):
         latency_e = latency
         latency_f = 0
-        correct_side = "e";
+        latency_side = "e";
     else:
         latency_e = 0
         latency_f = latency
-        correct_side = "f";
+        latency_side = "f";
     
 
 
